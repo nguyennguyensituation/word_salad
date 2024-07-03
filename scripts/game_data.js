8950c42a633cf6acf9566fd760978a06ce69f016
Tram Nguyen <designertvn@gmail.com>
2024-07-02 23:29:18 -0500

    Add 2 more objects to GAME_DATA

--- scripts/game_data.js
+++ scripts/game_data.js
@@ -43,5 +43,95 @@ const GAME_DATA = [
         ]
       },
     ]
+  },
+  {
+    id: 2,
+    categories: [
+      {
+        difficulty: 1,
+        categoryName: "latin words",
+        categoryWords: [
+          { wordValue: 'carpe', miniPuzzle: 'wordle'},
+          { wordValue: 'ergo', miniPuzzle: 'none' },
+          { wordValue: 'quid', miniPuzzle: 'crossword', crosswordClue: "A pound across the pond, perhaps" },
+          { wordValue: 'vox', miniPuzzle: 'none'},
+        ]
+      },
+      {
+        difficulty: 2,
+        categoryName: "pack (away) for future use",
+        categoryWords: [
+          { wordValue: 'squirrel', miniPuzzle: 'crossword', crosswordClue: "____ Nut Zippers" },
+          { wordValue: 'stash', miniPuzzle: 'none'},
+          { wordValue: 'store', miniPuzzle: 'wordle'},
+          { wordValue: 'stow', miniPuzzle: 'none'},
+        ]
+      },
+      {
+        difficulty: 3,
+        categoryName: "_____ game",
+        categoryWords: [
+          { wordValue: 'arcade', miniPuzzle: 'crossword', crosswordClue: "Where you might find Ms. Pac-Man"},
+          { wordValue: 'blame', miniPuzzle: 'none' },
+          { wordValue: 'numbers', miniPuzzle: 'none' },
+          { wordValue: 'squid', miniPuzzle: 'wordle' },
+        ]
+      },
+      {
+        difficulty: 4,
+        categoryName: "adjectives for assets",
+        categoryWords: [
+          { wordValue: 'fixed', miniPuzzle: 'none' },
+          { wordValue: 'frozen', miniPuzzle: 'crossword', crosswordClue: "Ice cold" },
+          { wordValue: 'liquid', miniPuzzle: 'none' },
+          { wordValue: 'toxic', miniPuzzle: 'wordle'},
+        ]
+      },
+    ]
+  },
+  {
+    id: 3,
+    categories: [
+      {
+        difficulty: 1,
+        categoryName: "highly skilled",
+        categoryWords: [
+          { wordValue: 'ace', miniPuzzle: 'none', crosswordClue: "Part of a winning pair in blackjack"},
+          { wordValue: 'maestro', miniPuzzle: 'none'},
+          { wordValue: 'adept', miniPuzzle: 'wordle' },
+          { wordValue: 'hotshot', miniPuzzle: 'none'},
+        ]
+      },
+      {
+        difficulty: 2,
+        categoryName: "kinds of cake",
+        categoryWords: [
+          { wordValue: 'birthday', miniPuzzle: 'none' },
+          { wordValue: 'crumb', miniPuzzle: 'crossword', crosswordClue: "The soft part of bread"},
+          { wordValue: 'marble', miniPuzzle: 'none'},
+          { wordValue: 'pound', miniPuzzle: 'wordle'},
+        ]
+      },
+      {
+        difficulty: 3,
+        categoryName: "tangible",
+        categoryWords: [
+          { wordValue: 'concrete', miniPuzzle: 'none' },
+          { wordValue: 'material', miniPuzzle: 'crossword', crosswordClue: "Half of a moniker for Madonna"},
+          { wordValue: 'real', miniPuzzle: 'none' },
+          { wordValue: 'solid', miniPuzzle: 'wordle' },
+        ]
+      },
+      {
+        difficulty: 4,
+        categoryName: "things you can throw in metaphors",
+        categoryWords: [
+          { wordValue: 'curveball', miniPuzzle: 'none' },
+          { wordValue: 'party', miniPuzzle: 'wordle' },
+          { wordValue: 'tantrum', miniPuzzle: 'none' },
+          { wordValue: 'wrench', miniPuzzle: 'crossword', crosswordClue: "To pull or twist"},
+        ]
+      },
+    ]
   }
 ]
