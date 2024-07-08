8ccd053e965500af537f0543382c10e406554f94
Tram Nguyen <designertvn@gmail.com>
2024-07-08 12:32:09 -0500

    Extract animations to separate file

---
+++ scripts/animations.js
@@ -0,0 +1,24 @@
+function bounceAnimation(divs) {
+  let timer = 0;
+
+  divs.forEach(div => {
+    setTimeout(() => {
+      div.classList.add('bounce');
+    }, timer);
+
+    setTimeout(() => {
+      div.classList.remove('bounce');
+    }, timer + 1000);
+    timer += 100;
+  })
+}
+
+function shakeAnimation(divs) {
+  divs.forEach(div => {
+    div.classList.add('shake');
+
+    setTimeout(() => {
+      div.classList.remove('shake');
+    }, 500) 
+  });
+}
--- scripts/app.js
+++ scripts/app.js
@@ -1,28 +1,3 @@
-function bounceAnimation(divs) {
-  let timer = 0;
-
-  divs.forEach(div => {
-    setTimeout(() => {
-      div.classList.add('bounce');
-    }, timer);
-
-    setTimeout(() => {
-      div.classList.remove('bounce');
-    }, timer + 1000);
-    timer += 100;
-  })
-}
-
-function shakeAnimation(divs) {
-  divs.forEach(div => {
-    div.classList.add('shake');
-
-    setTimeout(() => {
-      div.classList.remove('shake');
-    }, 500) 
-  });
-}
-
 document.addEventListener('DOMContentLoaded', () => {
   const myGame = new Game;
   myGame.init();
