0857496fdcdaa7a1000064c6f5805077ac0042c3
Tram Nguyen <designertvn@gmail.com>
2024-09-18 12:06:20 -0500

    remove tests from isValidWordleWord

--- scripts/modules/wordle.js
+++ scripts/modules/wordle.js
@@ -59,22 +59,16 @@ class Wordle extends Puzzle {
 
     while (left <= right) {
       let mid = Math.floor((left + right) / 2);
-      console.log("test ==============>");
-      console.log("current mid:" + VALID_WORDLE_WORDS[mid]);
       if (VALID_WORDLE_WORDS[mid] === word) {
         return true;
       } else if (VALID_WORDLE_WORDS[mid] < word) {
-        console.log("check right side");
         left = mid + 1;
       } else {
-        console.log("check left side");
         right = mid - 1;
       }
     }
 
-    console.log("returning false")
     return false;
-    // return VALID_WORDLE_WORDS.includes(word);
   }
 
   // Return array of results for each guessed letter
